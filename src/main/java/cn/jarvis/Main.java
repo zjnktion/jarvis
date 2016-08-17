7eecd43e434d0b018be55e3334502dca7d5c07e6
zjnktion <zjnktion@163.com>
2016-08-17 17:10:06 +0800

    [object-polling] Concurrent try

--- src/main/java/cn/jarvis/Main.java
+++ src/main/java/cn/jarvis/Main.java
@@ -1,8 +1,23 @@
 package cn.jarvis;
 
+import java.util.concurrent.locks.LockSupport;
+
 /**
  * @author zhengjn
  */
 public class Main
 {
+
+    public static void main(String[] args)
+    {
+
+        LockSupport.unpark(Thread.currentThread());
+        LockSupport.unpark(Thread.currentThread());
+        LockSupport.unpark(Thread.currentThread());
+
+        LockSupport.park();
+        //LockSupport.park();
+
+        System.out.println("1");
+    }
 }
