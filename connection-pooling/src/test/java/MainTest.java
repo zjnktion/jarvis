3dea1854d967b5b78e2da9c5e2955af6c4b129b9
zjnktion <zjnktion@163.com>
2016-09-08 16:33:30 +0800

    [connection-polling] complete test include my、apache dbcp、c3p0、druid、HikariCP

--- connection-pooling/src/test/java/MainTest.java
+++ connection-pooling/src/test/java/MainTest.java
@@ -1,13 +1,16 @@
 import cn.jarvis.connection.pooling.PooledConnection;
 import cn.jarvis.connection.pooling.PoolingDataSource;
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidDataSourceFactory;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import com.zaxxer.hikari.HikariDataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import java.beans.PropertyVetoException;
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author zjnktion
@@ -23,18 +26,20 @@ public class MainTest
 
     static final BasicDataSource BDS;
     static final ComboPooledDataSource CPDS;
+    static final DruidDataSource DDS;
+    static final HikariDataSource HDS;
 
     static
     {
         PDS = new PoolingDataSource();
 //        PDS.setMaxBlockMillis(3000L);
-        PDS.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        PDS.setDriverClassName("com.mysql.jdbc.Driver");
         PDS.setUrl("jdbc:mysql://10.31.90.118:3306/jarvis?serverTimezone=UTC&characterEncoding=utf-8");
         PDS.setUsername("root");
         PDS.setPassword("!2D#34S3aA$");
 
         BDS = new BasicDataSource();
-        BDS.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        BDS.setDriverClassName("com.mysql.jdbc.Driver");
         BDS.setUrl("jdbc:mysql://10.31.90.118:3306/jarvis?serverTimezone=UTC&characterEncoding=utf-8");
         BDS.setUsername("root");
         BDS.setPassword("!2D#34S3aA$6");
@@ -42,7 +47,7 @@ public class MainTest
         CPDS = new ComboPooledDataSource();
         try
         {
-            CPDS.setDriverClass("com.mysql.cj.jdbc.Driver");
+            CPDS.setDriverClass("com.mysql.jdbc.Driver");
         }
         catch (PropertyVetoException e)
         {
@@ -52,13 +57,38 @@ public class MainTest
         CPDS.setJdbcUrl("jdbc:mysql://10.31.90.118:3306/jarvis?serverTimezone=UTC&characterEncoding=utf-8");
         CPDS.setUser("root");
         CPDS.setPassword("!2D#34S3aA$");
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put(DruidDataSourceFactory.PROP_DRIVERCLASSNAME, "com.mysql.jdbc.Driver");
+        map.put(DruidDataSourceFactory.PROP_URL, "jdbc:mysql://10.31.90.118:3306/jarvis?serverTimezone=UTC&characterEncoding=utf-8");
+        map.put(DruidDataSourceFactory.PROP_USERNAME, "zhengjn");
+        map.put(DruidDataSourceFactory.PROP_PASSWORD, "~!@atm@!~");
+        DruidDataSource temp = null;
+        try
+        {
+            temp = (DruidDataSource) DruidDataSourceFactory.createDataSource(map);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        DDS = temp;
+
+        HDS = new HikariDataSource();
+        HDS.setDriverClassName("com.mysql.jdbc.Driver");
+        HDS.setJdbcUrl("jdbc:mysql://10.31.90.118:3306/jarvis?serverTimezone=UTC&characterEncoding=utf-8");
+        HDS.setUsername("zhengjn");
+        HDS.setPassword("~!@atm@!~");
+        HDS.setMaximumPoolSize(8);
     }
 
     public static void main(String[] args)
     {
 //        QueryThread qt = new QueryThread();
 //        QueryThread1 qt = new QueryThread1();
-        QueryThread2 qt = new QueryThread2();
+//        QueryThread2 qt = new QueryThread2();
+        QueryThread3 qt = new QueryThread3();
+//        QueryThread4 qt = new QueryThread4();
 
         Thread[] ts = new Thread[TNUM];
         for (int i = 0; i < TNUM; i++)
@@ -144,6 +174,68 @@ public class MainTest
                 try
                 {
                     conn = CPDS.getConnection();
+//                    Statement stm = conn.createStatement();
+//                    ResultSet rs = stm.executeQuery(SQL);
+//                    System.err.println(Thread.currentThread().getName() + ":::" + conn + ":::" + "begin::::::::::::::::::::::::::::::::" + i);
+//                    while (rs.next())
+//                    {
+//                        System.err.println("id:" + rs.getString(1) + "===name:" + rs.getString(2));
+//                    }
+//                    System.err.println(Thread.currentThread().getName() + ":::" + conn + ":::" + "end::::::::::::::::::::::::::::::::" + i);
+                    Thread.yield();
+                    conn.close();
+                }
+                catch (SQLException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            long t = System.currentTimeMillis() - time;
+            System.err.println(t);
+        }
+    }
+
+    static class QueryThread3 implements Runnable
+    {
+        public void run()
+        {
+            for (int i = 0; i < LNUM; i++)
+            {
+                Connection conn = null;
+                try
+                {
+                    conn = DDS.getConnection();
+//                    Statement stm = conn.createStatement();
+//                    ResultSet rs = stm.executeQuery(SQL);
+//                    System.err.println(Thread.currentThread().getName() + ":::" + conn + ":::" + "begin::::::::::::::::::::::::::::::::" + i);
+//                    while (rs.next())
+//                    {
+//                        System.err.println("id:" + rs.getString(1) + "===name:" + rs.getString(2));
+//                    }
+//                    System.err.println(Thread.currentThread().getName() + ":::" + conn + ":::" + "end::::::::::::::::::::::::::::::::" + i);
+                    Thread.yield();
+                    conn.close();
+                }
+                catch (SQLException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            long t = System.currentTimeMillis() - time;
+            System.err.println(t);
+        }
+    }
+
+    static class QueryThread4 implements Runnable
+    {
+        public void run()
+        {
+            for (int i = 0; i < LNUM; i++)
+            {
+                Connection conn = null;
+                try
+                {
+                    conn = HDS.getConnection();
 //                    Statement stm = conn.createStatement();
 //                    ResultSet rs = stm.executeQuery(SQL);
 //                    System.err.println(Thread.currentThread().getName() + ":::" + conn + ":::" + "begin::::::::::::::::::::::::::::::::" + i);
